6477946cbc0150efde1b7a5cce2aabab8f118ab3
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2024-08-13 16:51:19 +0300

    feat(enums): add StatusEnum and PriorityEnum for task management

---
+++ src/enums/taskEnum.ts
@@ -0,0 +1,11 @@
+export enum StatusEnum {
+  PENDING = 'pending',
+  INPROGRESS = 'in-progress',
+  COMPLETED = 'completed'
+};
+
+export enum PriorityEnum {
+  LOW = 'low',
+  MEDIUM = 'medium',
+  HIGH = 'high'
+}
